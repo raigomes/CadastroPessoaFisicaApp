79b53a4a56964c1d13cee0c6bd340c9b32019470
raigomes <rai.gomes.ismart@gmail.com>
2018-06-24 14:52:43 -0300

    Initial commit

---
+++ app/src/main/java/com/ifsp/rai/cadastropessoafisica/TelaInicialActivity.java
@@ -0,0 +1,13 @@
+package com.ifsp.rai.cadastropessoafisica;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TelaInicialActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_inicial);
+    }
+}
