a62fa3ec74a06932d1575050e28cf49f81148c68
raigomes <rai.gomes.ismart@gmail.com>
2018-06-25 01:14:12 -0300

    Implementando navegação entre telas

--- app/src/main/java/com/ifsp/rai/cadastropessoafisica/MainFormActivity.java
+++ app/src/main/java/com/ifsp/rai/cadastropessoafisica/MainFormActivity.java
@@ -1,13 +1,36 @@
 package com.ifsp.rai.cadastropessoafisica;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainFormActivity extends AppCompatActivity {
+
+    Button btVoltar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_form);
+
+        btVoltar = findViewById(R.id.btvoltar);
+
+        btVoltar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                voltar();
+            }
+        });
     }
+
+    private void voltar() {
+        Intent intent = new Intent();
+        intent.setClass(MainFormActivity.this, TelaInicialActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+
 }
--- app/src/main/java/com/ifsp/rai/cadastropessoafisica/TelaInicialActivity.java
+++ app/src/main/java/com/ifsp/rai/cadastropessoafisica/TelaInicialActivity.java
@@ -1,13 +1,34 @@
 package com.ifsp.rai.cadastropessoafisica;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class TelaInicialActivity extends AppCompatActivity {
+
+    Button btInsert;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_inicial);
+
+        btInsert = (Button) findViewById(R.id.btinsere);
+
+        btInsert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                chamaSegundaTela();
+            }
+        });
+    }
+
+    private void chamaSegundaTela() {
+        Intent intent = new Intent();
+        intent.setClass(TelaInicialActivity.this, MainFormActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
